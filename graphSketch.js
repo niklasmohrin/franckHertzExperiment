b280d9925ababc32e161cdd5184f1189e02a5f77
Niklas Mohrin <niklas.mohrin@gmail.com>
2019-02-15 22:39:14 +0100

    added graph axis, axis legend, graph resize event added graph point saving and screen size adapting

--- graphSketch.js
+++ graphSketch.js
@@ -2,6 +2,14 @@
 
 const GRAPH_STROKE = "blue";
 const GRAPH_SW = 2;
+const GRAPH_AXIS_STROKE = "white";
+const GRAPH_AXIS_SW = 3;
+const GRAPH_PADDING_HEIGHT = 0.15;
+const GRAPH_PADDING_WIDTH = 0.1;
+const GRAPH_AXIS_STEP = 5;
+const GRAPH_AXIS_LINE_HEIGHT = 2;
+const GRAPH_AXIS_LINE_SW = 2;
+const GRAPH_AXIS_FONT_SIZE = 10;
 
 const graphSketch = function(p) {
 	p.reset = () => {
@@ -11,13 +19,59 @@ const graphSketch = function(p) {
 		p.graphCnv.id("graph-canvas");
 
 		p.background(0);
+
+		p.stroke(GRAPH_AXIS_STROKE);
+		p.strokeWeight(GRAPH_AXIS_SW);
+		p.noFill();
+
+		p.line(
+			GRAPH_PADDING_WIDTH * p.width - GRAPH_SW,
+			p.height * (1 - GRAPH_PADDING_HEIGHT) + GRAPH_SW,
+			p.width * (1 - GRAPH_PADDING_WIDTH) - GRAPH_SW,
+			p.height * (1 - GRAPH_PADDING_HEIGHT) + GRAPH_SW
+		);
+		p.line(
+			GRAPH_PADDING_WIDTH * p.width - GRAPH_SW,
+			GRAPH_PADDING_HEIGHT * p.height + GRAPH_SW,
+			GRAPH_PADDING_WIDTH * p.width - GRAPH_SW,
+			p.height * (1 - GRAPH_PADDING_HEIGHT) + GRAPH_SW
+		);
+
+		p.strokeWeight(GRAPH_AXIS_LINE_SW);
+		p.textSize(GRAPH_AXIS_FONT_SIZE);
+		p.textAlign(p.CENTER, p.CENTER);
+
+		let y = p.height * (1 - GRAPH_PADDING_HEIGHT) + GRAPH_SW;
+		for (let i = 0; i <= GRID_MAX; i += GRAPH_AXIS_STEP) {
+			let x = map(
+				i,
+				0,
+				GRID_MAX,
+				GRAPH_PADDING_WIDTH * p.width,
+				p.width * (1 - GRAPH_PADDING_WIDTH)
+			);
+			if (i > 0)
+				p.line(x, y - GRAPH_AXIS_LINE_HEIGHT, x, y + GRAPH_AXIS_LINE_HEIGHT);
+
+			p.text(i, x, y + GRAPH_AXIS_LINE_HEIGHT + GRAPH_AXIS_FONT_SIZE);
+		}
+
 		p.stroke(GRAPH_STROKE);
 		p.strokeWeight(GRAPH_SW);
-		p.noFill();
+
+		p.points.forEach(point => {
+			point[0] *= p.parent.clientWidth / p.prevWidth;
+			point[1] *= p.parent.clientHeight / p.prevHeight;
+			p.point(Math.floor(point[0]), Math.floor(point[1]));
+		});
+
+		p.prevWidth = p.parent.clientWidth;
+		p.prevHeight = p.parent.clientHeight;
 	};
 
 	p.setup = () => {
 		p.parent = window.document.getElementById("graph-canvas-container");
+		p.points = [];
 		p.reset();
 	};
 
@@ -27,13 +81,26 @@ const graphSketch = function(p) {
 const graphP5 = new p5(graphSketch, "#graph-canvas-container");
 
 const drawOnGraph = () => {
-	graphP5.point(
-		Math.floor(map(uGrid, 0, GRID_MAX, 0, graphP5.width)),
-		Math.floor(map(f(uGrid), 0, f(GRID_MAX), graphP5.height, 0))
-	);
-	// console.log(
-	// 	"drew point at",
-	// 	map(uGrid, 0, GRID_MAX, 0, graphP5.width),
-	// 	map(f(uGrid), 0, f(GRID_MAX), graphP5.height, 0)
-	// );
+	const point = ([x, y] = [
+		Math.floor(
+			map(
+				uGrid,
+				0,
+				GRID_MAX,
+				GRAPH_PADDING_WIDTH * graphP5.width,
+				graphP5.width
+			)
+		),
+		Math.floor(
+			map(
+				f(uGrid),
+				0,
+				f(GRID_MAX),
+				graphP5.height - GRAPH_PADDING_HEIGHT * graphP5.height,
+				0
+			)
+		)
+	]);
+	graphP5.point(x, y);
+	graphP5.points.push(point);
 };
